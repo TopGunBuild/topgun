1c27a37f56d3be4eb102f2206e1b0a64c26f4e9c
kiborg <easysolpro@gmail.com>
2023-04-15 21:43:25 +0300

    chore: update package building process

---
+++ tools/prepare-package.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const PACKAGE_DIRS = ['client', 'server'];
+
+const getContent = function (name)
+{
+    return `{
+    "name": "topgun/${name}",
+    "typings": "../dist/${name}.d.ts",
+    "main": "../dist/${name}.js",
+    "module": "../dist/${name}.mjs",
+    "sideEffects": false
+}`;
+};
+
+for (const dir of PACKAGE_DIRS)
+{
+    if (!fs.existsSync(dir))
+    {
+        fs.mkdirSync(dir);
+        fs.writeFileSync(path.join(__dirname, '..', dir, 'package.json'), getContent(dir));
+    }
+}
