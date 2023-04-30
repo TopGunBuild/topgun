12f029136f6ce7a0a2c4a72af74587d55f5c918e
kiborg <easysolpro@gmail.com>
2023-04-30 10:33:02 +0300

    test: add test for graph validator

---
+++ tests/test-util.ts
@@ -0,0 +1,18 @@
+import { StructError, StructErrorInfo } from 'topgun-typed';
+
+export function expectOk(actual: any, expected: any) {
+    expect(actual).toEqual({ ok: true, value: expected });
+}
+
+export function expectErr(
+    actual: any,
+    message: string,
+    info?: StructErrorInfo,
+) {
+    expect(actual.ok).toBe(false);
+    expect(actual.error).toBeInstanceOf(StructError);
+    expect(actual.error.message).toEqual(message);
+    if (info) {
+        expect(actual.error.info).toEqual(expect.objectContaining(info));
+    }
+}
