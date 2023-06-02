274448df6e66dee87f9e47b797cc505723171cba
kiborg <easysolpro@gmail.com>
2023-06-02 07:18:19 +0300

    chore(test): lex test

--- tests/lex.test.ts
+++ tests/lex.test.ts
@@ -1,25 +1,92 @@
-import { TGClient, TGOptionsGet } from '../src/client';
+import { TGClient, TGGraphAdapter, TGGraphData, TGOptionsGet } from '../src/client';
 import { createServer } from '../src/server';
+
+let state = {
+    'xxx': {
+        _   : {
+            '#': 'xxx',
+            '>': {
+                name: 1682701808609
+            }
+        },
+        name: 'a'
+    },
+    'yyy': {
+        _   : {
+            '#': 'yyy',
+            '>': {
+                name: 1682701808609
+            }
+        },
+        name: 'b'
+    }
+};
 
 function get(
     nodeSoul: string,
     opts?: TGOptionsGet
-)
+): Promise<TGGraphData>
 {
-
+    console.log({ nodeSoul, opts });
+    return Promise.resolve(state);
 }
 
-const port   = 3457;
-const client = new TGClient({
-    peers         : [`http://127.0.0.1:${port}`],
-    persistSession: false
+function put(data: TGGraphData): Promise<TGGraphData>
+{
+    state = { ...state, ...data };
+    return Promise.resolve(state);
+}
+
+function adapter(): TGGraphAdapter
+{
+    return {
+        get: (soul: string, opts?: TGOptionsGet) => get(soul, opts),
+        put: (graphData: TGGraphData) => put(graphData),
+    };
+}
+
+const port = 3457;
+
+beforeEach(async () =>
+{
+
 });
-const server = createServer({ port });
 
 describe('LEX', () =>
 {
-    it('', async () =>
+    it('test lex query', async () =>
     {
+        const client = new TGClient({
+            peers         : [`http://127.0.0.1:${port}`],
+            persistSession: false
+        });
+        const server = createServer({
+            port,
+            adapter: adapter()
+        });
 
+        client.get('xxx').put({
+            value: 'yyy'
+        });
+
+        client
+            .get('xxx')
+            .once((data, id) =>
+            {
+                console.log(data);
+                expect(client).not.toBeUndefined();
+            });
+
+        /* client
+             .get('xxx')
+             .start('a')
+             .end('b')
+             .limit(2)
+             .map()
+             .once((data, id) =>
+             {
+                 console.log(data);
+                 expect(client).not.toBeUndefined();
+             });*/
     });
 });
