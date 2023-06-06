7eccef2bb4f980c3e5815431aff1ac41016e0e8d
kiborg <easysolpro@gmail.com>
2023-06-06 07:56:38 +0300

    chore(memory-adapter): add memory storage

---
+++ src/memory-adapter/memory-storage.ts
@@ -0,0 +1,30 @@
+import { TGNode } from '../types';
+
+export class MemoryStorage
+{
+    constructor(
+        protected map = new Map<string, TGNode>(),
+    )
+    {
+    }
+
+    putSync(key: string, value: TGNode): void
+    {
+        this.map.set(key, value);
+    }
+
+    getSync(key: string): TGNode|null
+    {
+        return this.map.get(key) || null;
+    }
+
+    put(key: string, value: TGNode): Promise<void>
+    {
+        return Promise.resolve(this.putSync(key, value));
+    }
+
+    get(key: string): Promise<TGNode>
+    {
+        return Promise.resolve(this.getSync(key));
+    }
+}
