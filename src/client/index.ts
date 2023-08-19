ec3537b81101cf3d107f1106af137c9bb5cb205e
kiborg <easysolpro@gmail.com>
2023-08-19 20:41:13 +0300

    fix(client): remove packages export

--- src/client/index.ts
+++ src/client/index.ts
@@ -9,9 +9,6 @@ export { TGGraphConnector } from './transports/graph-connector';
 export { TGGraphWireConnector } from './transports/graph-wire-connector';
 export { TGGraphConnectorFromAdapter } from './transports/graph-connector-from-adapter';
 export { TGUserApi } from './user-api';
-export * from '../crdt';
-export * from '../types';
-export * from '../sea';
-export * from '../stream';
+
 
 
