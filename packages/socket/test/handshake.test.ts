e836e6720560087fa429b7f199cbc8add690f0f2
kiborg <easysolpro@gmail.com>
2024-05-14 20:29:42 +0300

    test(socket): add handshake test

---
+++ packages/socket/test/handshake.test.ts
@@ -0,0 +1,31 @@
+import { cleanupTasks, randomPort } from '@topgunbuild/test-utils';
+import { ClientSocket, create, listen, SocketServer } from '../src';
+
+let server: SocketServer, client: ClientSocket;
+
+beforeEach(async () =>
+{
+    const port = await randomPort();
+    server = listen(port, {
+        wsEngine: 'ws'
+    });
+    client = create({
+        hostname: '127.0.0.1',
+        port
+    });
+});
+
+afterEach(async () =>
+{
+    await cleanupTasks(client, server);
+});
+
+describe('Socket handshake', () =>
+{
+    it('Exchange is attached to socket before the handshake event is triggered', async () =>
+    {
+        await server.listener('ready').once();
+        let { socket } = await server.listener('handshake').once();
+        expect(socket.exchange).not.toEqual(null);
+    });
+});
