faf96473ba4920dd93c10605e59b6db120add9c9
kiborg <easysolpro@gmail.com>
2024-05-15 16:10:14 +0300

    test(socket): add creation tests

---
+++ packages/socket/test/creation.test.ts
@@ -0,0 +1,43 @@
+import { cleanupTasks, randomPort } from '@topgunbuild/test-utils';
+import { ClientSocket, create, SocketClientOptions } from '../src';
+
+let client: ClientSocket, clientOptions: SocketClientOptions;
+
+afterEach(async () =>
+{
+    await cleanupTasks(client);
+});
+
+describe('Creation', () =>
+{
+    it(
+        'Should automatically connect socket on creation by default',
+        async () =>
+        {
+            clientOptions = {
+                hostname: '127.0.0.1',
+                port    : await randomPort(),
+            };
+
+            client = create(clientOptions);
+
+            expect(client.state).toEqual(ClientSocket.CONNECTING);
+        },
+    );
+
+    it(
+        'Should not automatically connect socket if autoConnect is set to false',
+        async () =>
+        {
+            clientOptions = {
+                hostname   : '127.0.0.1',
+                port       : await randomPort(),
+                autoConnect: false,
+            };
+
+            client = create(clientOptions);
+
+            expect(client.state).toEqual(ClientSocket.CLOSED);
+        },
+    );
+});
