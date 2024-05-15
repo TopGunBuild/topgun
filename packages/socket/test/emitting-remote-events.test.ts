1b3c93e952c7963a94df8755be516fe42f973a02
kiborg <easysolpro@gmail.com>
2024-05-15 18:30:50 +0300

    test(socket): add errors, order-events, emitting-remote-events tests

---
+++ packages/socket/test/emitting-remote-events.test.ts
@@ -0,0 +1,60 @@
+import { cleanupTasks, randomPort, wait } from '@topgunbuild/test-utils';
+import { ClientSocket, create, listen, SocketServer } from '../src';
+
+let server: SocketServer, client: ClientSocket;
+
+beforeEach(async () =>
+{
+    const port = await randomPort();
+    server     = listen(port, {
+        ackTimeout: 200,
+    });
+    client     = create({
+        hostname  : '127.0.0.1',
+        port,
+        ackTimeout: 200,
+    });
+
+    await server.listener('ready').once();
+});
+
+afterEach(async () =>
+{
+    await cleanupTasks(client, server);
+});
+
+describe('Emitting remote events', () =>
+{
+    it(
+        'Should not throw error on socket if ackTimeout elapses before response to event is sent back',
+        async () =>
+        {
+            let caughtError: Error, responseError: Error;
+
+            for await (let _ of client.listener('connect'))
+            {
+                try
+                {
+                    await client.invoke('performTask', 123);
+                }
+                catch (err)
+                {
+                    responseError = err;
+                }
+                await wait(250);
+                try
+                {
+                    client.disconnect();
+                }
+                catch (err)
+                {
+                    caughtError = err;
+                }
+                break;
+            }
+
+            expect(responseError).not.toEqual(null);
+            expect(caughtError).toBeUndefined();
+        },
+    );
+});
